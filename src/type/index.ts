ac428c98b5d290a76ef4698067126a7d25d3f0b8
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-17 13:47:04 +0900

    타입

---
+++ src/type/index.ts
@@ -0,0 +1,15 @@
+export interface KakaoPlace {
+  place_name: string;
+  address_name: string;
+  road_address_name: string;
+  x: string;
+  y: string;
+}
+export interface LocationData {
+  name: string;
+  address: string;
+  lat: number;
+  lng: number;
+  nx: number;
+  ny: number;
+}
