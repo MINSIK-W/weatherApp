901f7ef0127422285a8a5c5a085e380183ecafc3
MINSIK-W <108776722+MINSIK-W@users.noreply.github.com>
2025-08-21 12:49:38 +0900

    주소 변환 함수 추가

--- src/utils/kakaoApi.ts
+++ src/utils/kakaoApi.ts
@@ -8,6 +8,13 @@ interface SearchResult {
   longitude: number;
 }
 
+interface AddressInfo {
+  cityOrProvince: string; // 시도
+  district: string; // 시군구
+  town: string; // 읍면동
+  fullAddress: string; // 전체 주소
+}
+
 // 카카오 API 원본 타입
 interface KakaoResponse {
   documents: Array<{
@@ -15,6 +22,16 @@ interface KakaoResponse {
     address_name: string;
     x: string; // 경도
     y: string; // 위도
+  }>;
+}
+interface KaKaoReverseResponse {
+  documents: Array<{
+    address: {
+      region_1depth_name: string; // 시도
+      region_2depth_name: string; // 시군구
+      region_3depth_name: string; // 읍면동
+      address_name: string; // 전체 주소
+    };
   }>;
 }
 
@@ -46,5 +63,40 @@ export const searchLocation = async (query: string): Promise<SearchResult[]> => 
   } catch (error) {
     console.error('위치 검색 오류:', error);
     throw new Error('위치를 검색할 수 없습니다');
+  }
+};
+
+export const getAddressFrom = async (latitude: number, longitude: number): Promise<AddressInfo> => {
+  try {
+    console.log('좌표를 주소로 변환', { latitude, longitude });
+
+    const res = await fetch(
+      `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${longitude}&y=${latitude}&input_coord=WGS84`,
+      {
+        headers: {
+          Authorization: `KakaoAK ${config.KAKAO_API_KEY}`,
+        },
+      }
+    );
+
+    if (!res.ok) throw new Error(`주소 변환 실패: ${res.status}`);
+
+    const data: KaKaoReverseResponse = await res.json();
+    if (!data.documents || data.documents.length === 0)
+      throw new Error('해당 위치의 주소를 찾을 수 없습니다');
+
+    const address = data.documents[0].address;
+
+    const addressInfo: AddressInfo = {
+      cityOrProvince: address.region_1depth_name,
+      district: address.region_2depth_name,
+      town: address.region_3depth_name,
+      fullAddress: address.address_name,
+    };
+    console.log('변환 성공:', addressInfo);
+    return addressInfo;
+  } catch (err) {
+    console.error('변환 오류:', err);
+    throw new Error('주소를 가져올 수 없습니다');
   }
 };
